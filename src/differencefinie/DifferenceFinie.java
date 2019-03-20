5fe55433fc40f81dff69a653f856298b75e0827c
Yvan Tchoffo <yvan.tchoffo@yahoo.com>
2019-03-20 15:15:52 +0100

    implémentation des méthodes

--- src/differencefinie/DifferenceFinie.java
+++ src/differencefinie/DifferenceFinie.java
@@ -5,7 +5,8 @@
  */
 package differencefinie;
 
-
+import java.util.ArrayList;
+import solver.*;
 /**
  *
  * @author Yvan TCHOFFO
@@ -17,7 +18,14 @@ public class DifferenceFinie {
      */
     public static void main(String[] args)throws Exception {
         // TODO code application logic here
-        
+        DefaultFunction f = new DefaultFunction(0.0);
+        Power g = new Power(2);
+        int n = 101;
+        SimpleSolver solver = new SimpleSolver();
+        ArrayList<Double> solution1 = solver.solve(n,f.values(n),0,1);
+        for(int i=0; i!=solution1.size();i++){
+            System.out.println(solution1.get(i));
+        }
     }
     
 }
